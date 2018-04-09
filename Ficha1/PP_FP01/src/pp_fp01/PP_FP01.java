af5819ad398c0747a94c97f4a56e9efdced61b1c
Furia4 <isabel.6.066@gmail.com>
2018-04-09 23:54:09 +0100

    new commit

---
+++ Ficha1/PP_FP01/src/pp_fp01/PP_FP01.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pp_fp01;
+
+/**
+ * ESTGF- Escola Superior de Tecnologia e Gestão de Felgueiras
+ * IPP - Instituto Politécnico do Porto
+ * LSIRC - Licenciatura em Segurança e Informática em Redes de Computadores
+ * PP - Paradigmas de Programação
+ * <br>
+ * This class implements a simple first sample program in Java
+ * @author Isabel Silva
+ */
+public class PP_FP01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int numero = 10;
+        char caracter = 'A';
+        System.out.println("Saudações javanesas");
+    }
+    
+}
